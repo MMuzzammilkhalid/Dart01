7c903c71757df2a702e0de841464b54b3571fc96
muzammilhadiqa901@gmail.com <abdullahbinaslam64@gmail.com>
2024-08-25 13:16:00 +0500

    first commit

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  String UserName = "Muhammad";
+  String email = "muzammil@gmail.com";
+  String password = "12345";
+  print("Email: $email");
+  print("Pass: $password");
+  print("Username: $UserName");
+  print((UserName == "Muhammad" || email == "muzamil@gmail.com") &&
+  (password == "12345"));
+
+}
